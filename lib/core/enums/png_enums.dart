667a92fa08975713ad7963d0b6ba51bdb590ab06
onurcankurum <k.onurcan100@gmail.com>
2023-12-23 19:20:30 +0300

    PNG added

---
+++ lib/core/enums/png_enums.dart
@@ -0,0 +1,10 @@
+enum PngEnums {
+  bottomNavigationBarBackground;
+
+  String get path {
+    switch (this) {
+      case bottomNavigationBarBackground:
+        return 'assets/png/bottom_navigation_bar_background.png';
+    }
+  }
+}
