dc285d0f06efce0d0f97e8a62fb86471f6fb546d
onurcankurum <k.onurcan100@gmail.com>
2023-12-23 19:21:25 +0300

    base state added

--- lib/screens/mixin/base_view.dart
+++ lib/screens/mixin/base_view.dart
@@ -4,3 +4,10 @@ import '../../core/navigation/navigation/navigation_constants.dart';
 mixin BaseView on Widget {
   NavigationConstant get navigationConstant;
 }
+
+mixin BaseStateMixin<T extends StatefulWidget> on State<T> {
+  double dynamicHeight(double value) =>
+      MediaQuery.of(context).size.height * value;
+  double dynamicWidth(double value) =>
+      MediaQuery.of(context).size.width * value;
+}
