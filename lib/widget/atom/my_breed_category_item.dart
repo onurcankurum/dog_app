cf9a496c22ed17b707c7eca125b0f8be4cbadbf0
onurcankurum <k.onurcan100@gmail.com>
2023-12-25 01:47:21 +0300

    breed_category_item widget added

---
+++ lib/widget/atom/my_breed_category_item.dart
@@ -0,0 +1,78 @@
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dog_app/core/extension/theme_extension.dart';
+import 'package:flutter/material.dart';
+
+class MyBreedCategoryItem extends StatelessWidget {
+  const MyBreedCategoryItem(
+      {super.key, required this.imageUrl, required this.text});
+  final String imageUrl;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          CachedNetworkImage(
+            imageUrl: imageUrl,
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: context.myTheme.breedCategoryItemBorderColor,
+                  width: 1,
+                ),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+            placeholder: (context, url) => const Center(
+              child: SizedBox(
+                  height: 30, width: 30, child: CircularProgressIndicator()),
+            ),
+            errorWidget: (context, url, error) {
+              return const Center(child: Icon(Icons.error));
+            },
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  height: 38,
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(8),
+                        topRight: Radius.circular(8),
+                      ),
+                      color:
+                          context.myTheme.breedCategoryItemTextBackgroundColor),
+                  child: Center(
+                    child: Text(
+                      text,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: context.myTheme.breedCategoryItemTextColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
