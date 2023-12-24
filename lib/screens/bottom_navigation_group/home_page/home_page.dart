8de79602a0cfc4e3437a3f13040a6a9572c30f45
onurcankurum <k.onurcan100@gmail.com>
2023-12-24 03:30:18 +0300

    fix

--- lib/screens/bottom_navigation_group/home_page/home_page.dart
+++ lib/screens/bottom_navigation_group/home_page/home_page.dart
@@ -17,49 +17,26 @@ final class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Dog App'),
+      ),
       body: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                GestureDetector(
-                  onTap: () async {},
-                  child: Container(
-                    height: 233,
-                    width: double.infinity,
-                    color: Colors.yellow,
-                  ),
+          GridView.count(
+            crossAxisCount: 2,
+            children: List.generate(100, (index) {
+              return Center(
+                child: Text(
+                  'Item $index',
+                  style: Theme.of(context).textTheme.headlineSmall,
                 ),
-                Container(
-                  height: 233,
-                  width: double.infinity,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 233,
-                  width: double.infinity,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  height: 233,
-                  width: double.infinity,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 233,
-                  width: double.infinity,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  height: 233,
-                  width: double.infinity,
-                  color: Colors.red,
-                ),
-              ],
-            ),
+              );
+            }),
           ),
-          const Positioned(
-              bottom: 0, right: 0, left: 0, child: _ExpandableTextField())
+          Positioned(
+              bottom: 0, left: 0, right: 0, child: _ExpandableTextField())
         ],
       ),
     );
@@ -88,6 +65,7 @@ class _ExpandableTextFieldState extends State<_ExpandableTextField>
           minLines = 3;
         });
       } else {
+        if (_isExpanded) return;
         setState(() {
           minLines = 1;
         });
@@ -98,87 +76,86 @@ class _ExpandableTextFieldState extends State<_ExpandableTextField>
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GestureDetector(
-          onVerticalDragUpdate: (details) {
-            if (details.delta.dy > 0) {
-              // Aşağı doğru kaydırma (kapama)
-              setState(() {
-                _isExpanded = false;
-              });
-            } else if (details.delta.dy < 0) {
-              // Yukarı doğru kaydırma (açma)
-              setState(() {
-                _isExpanded = true;
-              });
-            }
-          },
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              AnimatedContainer(
-                  duration: const Duration(milliseconds: 500),
-                  height: _isExpanded ? 300 : null,
-                  constraints: const BoxConstraints(
-                    minHeight: 58,
-                    maxHeight: 300,
-                  ),
-                  margin: _focusNode.hasFocus
-                      ? EdgeInsets.zero
-                      : const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: const Radius.circular(8),
-                      topRight: const Radius.circular(8),
-                      bottomLeft: _focusNode.hasFocus
-                          ? Radius.zero
-                          : const Radius.circular(8),
-                      bottomRight: _focusNode.hasFocus
-                          ? Radius.zero
-                          : const Radius.circular(8),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 1,
-                        blurRadius: 8,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  width: double.infinity,
-                  child: TextField(
-                    focusNode: _focusNode,
-                    minLines: minLines,
-                    maxLines: 99,
-                    decoration: InputDecoration(
-                      hintText: 'Search',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  )),
-              if (_focusNode.hasFocus)
-                Positioned(
-                  top: 8,
-                  child: Container(
-                    height: 3,
-                    width: 24,
-                    decoration: BoxDecoration(
-                      color: themeData.dividerTheme.color,
-                      borderRadius: BorderRadius.circular(2),
-                    ),
-                  ),
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        if (details.delta.dy > 0) {
+          // Aşağı doğru kaydırma (kapama)
+          setState(() {
+            if (!_focusNode.hasFocus) minLines = 1;
+
+            _isExpanded = false;
+          });
+        } else if (details.delta.dy < 0) {
+          // Yukarı doğru kaydırma (açma)
+          if (!_focusNode.hasFocus) return;
+          setState(() {
+            _isExpanded = true;
+          });
+        }
+      },
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              height: _isExpanded ? double.infinity : null,
+              constraints: BoxConstraints(
+                minHeight: 58,
+                maxHeight: dynamicHeight(0.5),
+              ),
+              margin: _focusNode.hasFocus
+                  ? EdgeInsets.zero
+                  : const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(8),
+                  topRight: const Radius.circular(8),
+                  bottomLeft: _focusNode.hasFocus
+                      ? Radius.zero
+                      : const Radius.circular(8),
+                  bottomRight: _focusNode.hasFocus
+                      ? Radius.zero
+                      : const Radius.circular(8),
                 ),
-            ],
-          ),
-        ),
-      ],
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 8,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              width: double.infinity,
+              child: TextField(
+                focusNode: _focusNode,
+                minLines: minLines,
+                maxLines: 99,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              )),
+          if (_focusNode.hasFocus || _isExpanded)
+            Positioned(
+              top: 8,
+              child: Container(
+                height: 3,
+                width: 24,
+                decoration: BoxDecoration(
+                  color: themeData.dividerTheme.color,
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
